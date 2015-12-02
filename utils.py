fff0cd4d9c66fff45715b78797d07d030cb88241
Billy Rhoades <billyarhoades@gmail.com>
2015-12-02 09:55:23 -0600

    Added utils.py

---
+++ utils.py
@@ -0,0 +1,20 @@
+import tempfile
+import tarfile
+import os
+
+def make_gzip(f, name=None):
+    # Make a temporary (named) gzip file somewhere. 
+    file = tempfile.NamedTemporaryFile(delete=False)
+    file.close()
+    fn = file.name
+
+    with tarfile.open(fn, "w:gz") as t:
+        t.add(f)
+
+        print(t.list())
+
+    if name is not None:
+        fn = os.path.join(os.path.dirname(fn),name) + ".tar.gz"
+        os.rename(file.name, fn)
+                
+    return fn
