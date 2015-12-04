86f5526ab7f1db76bb9f5250f74d3ce915eb86a4
Billy Rhoades <billyarhoades@gmail.com>
2015-12-03 23:00:04 -0600

    Fixed new folder structure with argparse.
    Submodules were being prepended with grader (fixed).
    Just running it with no arguments also caused it to error out, that's
    been fixed now to print help.

--- grader/grader/__init__.py
+++ grader/grader/__init__.py
@@ -1,4 +1,6 @@
 import argparse
+import re
+import sys
 
 from grader import image, grade
 # This module delegates to submodules
@@ -7,14 +9,19 @@ submodules = [image, grade]
 
 def run():
     parser = argparse.ArgumentParser(description='Grading script. Fix this desc.')
-    subparsers = parser.add_subparsers(help="sub command help")
+    subparsers = parser.add_subparsers(help="additional help", 
+            description="valid subcommands", title="subcommands")
     for m in submodules:
-        subparser = subparsers.add_parser(m.__name__, help=m.help)
+        subparser = subparsers.add_parser(re.sub(r'^[^\.]+\.', '', m.__name__),
+                help=m.help)
         subparser.set_defaults(func=m.run.run_module)
         m.run.create_parser(subparser)
 
     args = parser.parse_args()
-    args.func(args)
+    if len(sys.argv) <= 1:
+        parser.print_help()
+    else:
+        args.func(args)
 
 if __name__ == '__main__':
     run()
