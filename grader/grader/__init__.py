cfa82087e0d651bdc936a7a98b0c9618c6159eec
Michael Wisely <michaelwisely@gmail.com>
2015-12-05 13:54:47 -0600

    Add a help command

--- grader/grader/__init__.py
+++ grader/grader/__init__.py
@@ -14,16 +14,22 @@ def run():
     parser = argparse.ArgumentParser(description=description)
     subparsers = parser.add_subparsers(title="subcommands")
 
+    # If no arguments are provided, show the usage screen
+    parser.set_defaults(run=lambda x: parser.print_usage())
+
+    # The 'help' command shows the help screen
+    help_parser = subparsers.add_parser("help", help="Show this help screen")
+    help_parser.set_defaults(run=lambda x: parser.print_help())
+
+    # Set up subcommands for each package
     for name, path in subcommands.items():
         module = importlib.import_module(path)
         subparser = subparsers.add_parser(name, help=module.help)
         module.setup_parser(subparser)
 
+    # Do it
     args = parser.parse_args()
-    if len(sys.argv) <= 1:
-        parser.print_help()
-    else:
-        args.run(args)
+    args.run(args)
 
 if __name__ == '__main__':
     run()
