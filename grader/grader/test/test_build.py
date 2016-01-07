35bcc48d2303f81276b048dd5806df74e54dfd4c
Michael Wisely <michaelwisely@gmail.com>
2016-01-07 00:33:26 -0600

    Clean up build test

--- grader/grader/test/test_build.py
+++ grader/grader/test/test_build.py
@@ -1,18 +1,28 @@
 import os
 import pytest
-import re
-import yaml
 
 from subprocess import Popen, PIPE
 
 
 def has_installed(program):
-    proc = Popen(["which", program],stdout=PIPE,stderr=PIPE)
+    """Checks to see if a program is installed using ``which``.
+
+    :param str program: the name of the program we're looking for
+
+    :rtype bool:
+
+    :return: True if it's installed, otherwise False.
+
+    """
+    proc = Popen(["which", program], stdout=PIPE, stderr=PIPE)
     exit_code = proc.wait()
     return exit_code == 0
 
+
 hasdocker = pytest.mark.skipif(not has_installed("docker"),
                                reason="Docker must be installed.")
+"""A decorator to skip a test if docker is not installed."""
+
 
 @hasdocker
 def test_build(parse_and_run):
@@ -22,7 +32,7 @@ def test_build(parse_and_run):
     parse_and_run(["new", "a1"])
 
     dockerfile_path = os.path.join(path, "assignments", "a1",
-                              "gradesheet", "Dockerfile")
+                                   "gradesheet", "Dockerfile")
     with open(dockerfile_path, 'w') as dockerfile:
         dockerfile.write("FROM ubuntu:12.04")
 
