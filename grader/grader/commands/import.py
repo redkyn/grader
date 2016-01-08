315cb32e8e58c6712475f4a3efe85a23451e5a7f
Michael Wisely <michaelwisely@gmail.com>
2016-01-08 15:36:19 -0600

    Catch SubmissionErrors on 'import'

--- grader/grader/commands/import.py
+++ grader/grader/commands/import.py
@@ -1,8 +1,8 @@
-'''TODO: Build command docs
+'''TODO: Import command docs
 '''
 import logging
 
-from grader.models import Grader, Submission, AssignmentError
+from grader.models import Grader, Submission, AssignmentError, SubmissionError
 from grader.utils.config import require_grader_config
 
 logger = logging.getLogger(__name__)
@@ -13,7 +13,7 @@ help = "Import student submission(s)"
 def setup_parser(parser):
     parser.add_argument('--kind', required=True,
                         choices=Submission.get_importers().keys())
-    parser.add_argument('name',
+    parser.add_argument('assignment',
                         help='Name of the assignment to add submission(s) to.')
     parser.add_argument('submission_path',
                         help='Path to the submission(s) to add.')
@@ -25,9 +25,14 @@ def run(args):
     g = Grader(args.path)
 
     try:
-        g.import_submission(args.name, args.submission_path, args.kind)
+        g.import_submission(args.assignment, args.submission_path, args.kind)
     except AssignmentError as e:
         logger.error(
-            "Couldn't build assignment: {}".format(e)
+            "Couldn't find assignment: {}".format(e)
+        )
+        raise SystemExit(1)
+    except SubmissionError as e:
+        logger.error(
+            "Couldn't import submission(s): {}".format(e)
         )
         raise SystemExit(1)
