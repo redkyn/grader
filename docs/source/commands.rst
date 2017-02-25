c4e1fb5a9a6db4968631c48de06ef41f7b63356a
Billy Rhoades <billyarhoades@gmail.com>
2017-02-25 14:32:42 -0600

    Inspect Subcommand - Enter Submission Container (#13)
    * Added inspect, launches you into a student's container.
    
    Currently needs some more work.
    
    * Now stopping container after inspection.
    
    Switching from execl to subprocess.call.
    
    * Ran flake8, fixed inspect/__init__.py formatting.
    
    Changed log_colors...
    
    * Added a selection prompt for multiple graded submissions.
    
    If otherwise it takes the first.
    
    * Flake8 changes again (trailing whitespace).
    
    Also added a few comments.
    
    * Review modifications
    
    - Nicer line breaking
    - Use `shutil.which`
    
    * Now using full_id instead of .id.
    
    Fixes inspect.
    
    * Add flag for user to inspect as
    
    * Added doc for inspect, changed user -> student_id
    
    * Added additional details about --user.
    
    * Minor grammatical changes in inspect docs.

--- docs/source/commands.rst
+++ docs/source/commands.rst
@@ -31,3 +31,43 @@ Commands
 
 ``grade``
 ---------
+
+.. _inspect:
+
+``inspect``
+---------
+Inspects a student's submission's docker container by launching an interactive shell
+within it. Handles both starting and stopping the container behind the scenes.
+
+Configuration
+*************
+
+Gradesheet Configuration Keys:
+    shell (*/bin/bash/*): The shell command to run interactively within the submission's container.
+
+Usage
+*****
+
+.. code-block:: bash
+
+  grader inspect [--user user] assignment student_id
+
+Grader inspect interactively launches into a specified program on a container.
+When the program termiantes, the container is stopped implicitly by Docker.
+
+An optional user can be specified for the process to be launched as. This is particularly
+useful when gradesheets restrict the container's default user's access to
+submission files which need modification.
+
+Examples
+********
+
+.. code-block:: bash
+
+  $ grader inspect 1 bjrq48
+  INFO Starting container bjrq48--eb86a392-a9f5-464b-a70e-15b9366e8550
+  student@9338726 ~ $ echo "Hello!"
+  Hello!
+  student@9338726 ~ $ exit
+  INFO Stopping container bjrq48--eb86a392-a9f5-464b-a70e-15b9366e8550
+  $
---
+++ grader/grader/commands/inspect.py
@@ -0,0 +1,71 @@
+import logging
+import shutil
+import subprocess
+
+from grader.models import Grader
+from grader.utils.config import require_grader_config
+
+logger = logging.getLogger(__name__)
+
+help = "Inspect a graded submission's container."
+
+
+def setup_parser(parser):
+    parser.add_argument('assignment',
+                        help='Name of the assignment which the user has a '
+                             'graded submission for.')
+    parser.add_argument('student_id',
+                        help='Inspect submission belonging to this student.')
+    parser.add_argument('--user',
+                        help="Linux user to inspect as")
+    parser.set_defaults(run=run)
+
+
+@require_grader_config
+def run(args):
+    g = Grader(args.path)
+    a = g.get_assignment(args.assignment)
+
+    if args.username not in a.submissions_by_user:
+        logger.error("User does not have a graded submission available.")
+        return
+
+    user_submissions = a.submissions_by_user[args.username]
+    id = None
+    # If they have multiple submissions, make them choose
+    if len(user_submissions) > 1:
+        i = 1
+        print("Index\tCreated")
+        for s in user_submissions:
+            info = a.docker_cli.inspect_container(s.full_id)
+            print("{0}\t{1}".format(i, info['Created']))
+            i += 1
+        choice = -1
+        while choice < 0 or choice >= len(user_submissions):
+            choice = input("Please enter your selection: ")
+            try:
+                choice = int(choice)-1
+            except:
+                choice = -1
+
+        id = user_submissions[choice].full_id
+    else:
+        id = user_submissions[0].full_id
+
+    shell = a.gradesheet.config.get('shell', '/bin/bash')
+
+    # Start container, run exec, stop container
+    logger.info("Starting container {0}".format(id))
+    a.docker_cli.start(container=id)
+
+    command = [shutil.which("docker"), "exec", "-it"]
+    if args.user:
+      command.append("-u")
+      command.append(args.user)
+
+    command.append(id)
+    command.append(shell)
+    subprocess.call(command)
+
+    logger.info("Stopping container {0}".format(id))
+    a.docker_cli.stop(container=id)
