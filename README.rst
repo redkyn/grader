55fb3f8eaab1d18a245ef01346684acba49364c9
linuxmercedes <linuxmercedes@gmail.com>
2019-01-02 19:47:55 -0600

    Prepare setup.py for pypi release

---
+++ README.rst
@@ -0,0 +1,53 @@
+.. image:: docs/grader.png
+    :alt: Grader
+
+|pypi| |travis|
+
+.. |pypi| image:: https://badge.fury.io/py/grader.svg
+    :target: https://badge.fury.io/py/grader
+    :alt: PyPi package
+
+.. |travis| image:: https://travis-ci.org/redkyn/grader.svg?branch=master
+    :target: https://travis-ci.org/redkyn/grader
+    :alt: Build status
+
+This program uses Docker and Python to easily grade many assignments in an encapsulated, safe manner. The general process starts by creating an image for a class or an assignment. That image can be given a payload, a protected grading script, which has a grading hooks. The grade module, when ran, creates an individual container for each student's assignment and runs a payload hook. The payload then returns a JSON response with stdin, stdout, and additional response information depending on the image ran.
+
+Setup
+-----
+
+This project is setup using `buildout <http://www.buildout.org/en/latest/>`_. It's super easy to get ``grader`` up and running:
+
+You'll probably need the following libraries...
+
+- build-essential
+- libncurses-dev
+- python3-dev
+
+And, of course, you'll want Docker if you plan to do anything interesting at all. Grader requires docker with **API version >= 1.20**. We use ``put_archive`` (``PUT /containers/(id)/archive``) from docker-py.
+
+.. highlight:: bash
+::
+
+  # Bootstrap buildout
+  python3 bin/bootstrap-buildout.py
+  
+  # Setup your development environment with buildout
+  bin/buildout
+
+Now you're done! When it sets up your environment, buildout creates a couple of executables and places them in ``bin/`` for you. They are:
+
+- **buildout** : That's buildout. Run it whenever you add eggs to ``grader/setup.py`` or modify ``buildout.cfg``
+- **grader** : That's ``grader``!
+- **python** : "What? Another Python!?" Yeah... This is an ipython interpreter. It's handy because it has access to all the same packages that were installed for ``grader``.
+
+Check your style
+----------------
+
+It's important to keep our code looking consistent. There's a tool installed by buildout called ``flake8`` that will check your Python source for issues in style. Simply run it like this:
+
+.. highlight:: bash
+::
+
+  # Make sure you run this in your grader directory. flake8 will recursively check source files for style issues.
+  bin/flake8 grader/
--- grader/setup.py
+++ grader/setup.py
@@ -1,10 +1,56 @@
-from setuptools import setup  # pragma: no cover
+from setuptools import setup, find_packages # pragma: no cover
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, '../README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(  # pragma: no cover
     name="grader",
-    version="0.0.1",  # http://semver.org/spec/v2.0.0.html
-    url='https://github.com/brhoades/grader',
     description="A grading framework for evaluating programming assignments",
+    long_description=long_description,
+
+    # Derive version from git tags
+    use_scm_version={"root": "..", "relative_to": __file__},
+
+    url='https://github.com/redkyn/grader',
+
+    author='B. Rhoades, N. Jarus, M. Wisely, & T. Morrow',
+
+    author_email='jarus@mst.edu',
+
+    # For a list of valid classifiers, see
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Education',
+        'Topic :: Education',
+        'Topic :: Software Development :: Version Control :: Git',
+        'Environment :: Console',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
     packages=['grader'],
     package_dir={'grader': 'grader'},
     install_requires=[
@@ -18,6 +64,17 @@ setup(  # pragma: no cover
         'Jinja2>=2.10',
         'redkyn-common>=1.0.1'
     ],
+
+    python_requires='>=3.4',
+    setup_requires=[
+        'setuptools_scm>=1.15',
+        'setuptools>=12'
+    ],
+
+    data_files=[
+        ('share/grader', ['misc/completions/_grader.zsh'])
+    ],
+
     entry_points={
         'console_scripts': ['grader = grader:run'],
     },
