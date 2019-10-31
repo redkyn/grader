a59473e57b631dc10d973378e241b91c6a93d576
linuxmercedes <linuxmercedes@gmail.com>
2019-10-31 09:32:35 -0500

    Rename to redkyn-grader since pypi already has a grader package

--- redkyn-grader/setup.py
+++ redkyn-grader/setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, '../README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(  # pragma: no cover
-    name="grader",
+    name="redkyn-grader",
     description="A grading framework for evaluating programming assignments",
     long_description=long_description,
     long_description_content_type="text/markdown",
